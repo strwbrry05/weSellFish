4b35441b62d8b6ed55528d8068aecca11fa45b58
strwbrry05 <jq.strwbrry05@gmail.com>
2025-05-19 15:18:51 -0700

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { FiGithub } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
+import { FaRegHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <p>STRWBRRY05</p>
+    <div className="p-10 bg-[var(--color-light-blue)] text-center md:p-4">
+      <p className="text-3xl md:text-2xl text-[var(--color-berekley-blue)] font-bold">
+        STRWBRRY05
+      </p>
       <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className="flex flex-row justify-center gap-3 md:mt-2 mt-4 mb-3">
+          <a href="#">
+            <li className="p-2 rounded-md bg-[var(--color-berekley-blue)]">
+              <FiGithub className="md:text-xl text-2xl text-[var(--color-white-smoke)]" />
+            </li>
+          </a>
+          <a href="#">
+            <li className="p-2 rounded-md bg-[var(--color-berekley-blue)]">
+              <FiLinkedin className="md:text-xl text-2xl text-[var(--color-white-smoke)]" />
+            </li>
+          </a>
+          <a href="#">
+            <li className="p-2 rounded-md bg-[var(--color-berekley-blue)]">
+              <FaRegHeart className="md:text-xl text-2xl text-[var(--color-white-smoke)]" />
+            </li>
+          </a>
         </ul>
       </div>
-      <p>&copy; 2025 Jacqueline Molina</p>
+      <p className="text-[var(--color-berekley-blue)]">
+        &copy;2025 Jacqueline Molina
+      </p>
     </div>
   );
 };
